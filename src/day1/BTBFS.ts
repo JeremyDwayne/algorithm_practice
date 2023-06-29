a82b6a27936d9192355802df4837268e67276907
Jeremy Winterberg <jeremy@jeremydwayne.com>
2023-06-28 22:47:17 -0500

    all the trees

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,18 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  // unshift and shift in typescript are O(n^2)
+  // using ArrayList [] for simplicity.
+  // If you used a proper Queue you build yourself it would be O(n)
+  const q: (BinaryNode<number> | null)[] = [head];
+  while (q.length) {
+    const curr = q.shift() as BinaryNode<number> | undefined | null;
+    if (!curr) continue;
 
+    if (curr.value === needle) return true;
+
+    q.push(curr.left);
+    q.push(curr.right);
+  }
+
+  return false;
 }
+
